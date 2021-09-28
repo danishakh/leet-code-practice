9699b7c97975c3b3de9c308de100528b0f84a070
Don <danishakh@gmail.com>
2021-09-28 10:42:47 -0700

    added LC 278 and 35

---
+++ 278-first-bad-version/first-bad-version.py
@@ -0,0 +1,42 @@
+# this problem is an extension of binary search
+def firstBadVersion(n):
+    if n < 2:
+        return n
+
+    start = 1
+    end = n
+
+    while start <= end:
+        mid = start + (end - start) // 2
+
+        if isBadVersion(mid) and not isBadVersion(mid-1):
+            return mid
+
+        # 1st bad version is to the left of mid
+        elif isBadVersion(mid-1):
+            end = mid - 1
+
+        # not isBadVersion(mid) - 1st bad version is to the right of mid
+        else:
+            start = mid + 1
+
+
+# another solution
+def firstBadVersion2(n):
+    if n < 2:
+        return n
+
+    start = 1
+    end = n
+    res = 0
+
+    while start <= end:
+        mid = start + (end - start) // 2
+
+        if isBadVersion(mid):
+            end = mid - 1
+        else:
+            start = mid + 1
+
+    # res will eventually end up at the 1st bad version position
+    return res
